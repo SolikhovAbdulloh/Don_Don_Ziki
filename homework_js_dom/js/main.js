6eb4f567f1e24066e1490fd7c9ce7547d6dd1d02
SolihovAbdulloh <abdullasolikh@mail.com>
2024-10-08 05:24:39 +0500

    js

--- homework_js_dom/js/main.js
+++ homework_js_dom/js/main.js
@@ -8,4 +8,33 @@ document.addEventListener("DOMContentLoaded",  () => {
   });
 });
 
+const buttons = document.querySelectorAll('.Accordion')
+ 
+buttons.forEach((btn) =>{
+    btn.addEventListener('click',()=>{
+const panel = btn.nextElementSibling;
+
+if (panel.style.display === "none") {
+  panel.style.display = "block";
+} else {
+  panel.style.display = "none";
+}
+    })
+})
+
+const rasmlar = document.querySelectorAll('.img')
+
+rasmlar.forEach((rasm)=>{
+    let aylanish = 0;
+    rasm.addEventListener("click",()=>{
+        aylanish+=180
+
+        rasm.style.transform = `rotate(${aylanish}deg)`;
+    })
+})
+ 
+
+    
+    
+
 
