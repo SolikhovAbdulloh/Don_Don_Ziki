e8d2f5b22d8531241f97f6576cd55a951d0a4d74
SolihovAbdulloh <abdullasolikh@mail.com>
2024-10-18 09:50:37 +0500

    sd

--- o'yin_Don_Ziki/ziki.js
+++ o'yin_Don_Ziki/ziki.js
@@ -1,48 +1,49 @@
-const player = document.querySelector('#player')
-const computer = document.querySelector('#computer')
-const restart = document.querySelector('#restart')
-const icons = document.querySelectorAll('.icons i')
-const countComp = document.querySelector('#count1')
-const countPlyer = document.querySelector('#count2')
+const restart = document.querySelector("#restart");
+const icons = document.querySelectorAll(".icons i");
+const countComp = document.querySelector("#count1");
+const countPlyer = document.querySelector("#count2");
 
-restart.addEventListener('click',()=>{
-    countComp.textContent = 0
-    countPlyer.textContent = 0
-})
+restart.addEventListener("click", () => {
+  countComp.textContent = "0";
+  countPlyer.textContent = "0";
+});
 
-function camputerRandom(){
-    let rand = Math.ceil((Math.random() * 3))
-    if(rand === 1){
-        return "fa-solid fa-hand-back-fist"; //tosh
-    }else if(rand === 2){
-        return "fa-solid fa-hand-scissors"; //qaychi 
-    }else{
-        return "fa-solid fa-hand"; // qogoz
-    }
-    
-}
 icons.forEach((icon) => {
   icon.addEventListener("click", (e) => {
-    const player = e.target.classList[1]; 
-    const computer = camputerRandom();
-    solishtir(player, computer); 
-   
-    
+    const player = e.target.getAttribute("data-id");
+    // console.log(player);
+
+    const computer = computerRandom();
+    solishtir(player, computer);
   });
 });
 
+function solishtir(player, computer) {
+  console.log(player, computer);
+  if (player == computer) {
+    alert(`Durrang chunki kampyuter ${computer} qildi`);
+  } else if (
+    (player == "Qog'oz" && computer == "Tosh") ||
+    (player == "Qaychi" && computer == "Qog'oz") ||
+    (player == "Tosh" && computer == "Qaychi")
+  ) {
+    alert(`Urraa Yutdingiz Campyuter ${computer} qildi`);
+  } else {
+    alert(`Yutqazdingiz kampyuter ${computer} qildi`)
+  }
+    countPlyer = parseInt(countPlyer.textContent) + 1;
+    countComp = parseInt(countComp.textContent) + 1;
 
-function solishtir(player,computer){
-    if(player == computer){
-        alert('durrang')
-    }else if (
-      (player == "fa-solid fa-hand" && computer == "fa-solid fa-hand-back-fist") ||
-      (player == "fa-solid fa-hand-scissors " && computer == "fa-solid fa-hand") ||
-      (player == "fa-solid fa-hand-back-fist" && computer == "fa-solid fa-hand-scissors ")
-    ) {
-        countPlyer.textContent = parseInt(countPlyer.textContent) +1
-    }else{
+}
 
-        countComp.textContent = parseInt(countComp.textContent) + 1
-    }
+function computerRandom() {
+  let rand = Math.ceil(Math.random() * 3);
+
+  if (rand === 1) {
+    return "Tosh"; // Tosh
+  } else if (rand === 2) {
+    return "Qaychi"; // Qaychi
+  } else {
+    return "Qog'oz"; // Qog'oz
+  }
 }
